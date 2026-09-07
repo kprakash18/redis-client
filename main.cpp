214d5d6c649604d3819189a445a67bd92977f02a
kprakash18 <iamkprakash18@gmail.com>
2026-09-07 17:42:45 +0530

    write out TODO list for project

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/*
+1. Command-Line argument parsing
+    -h <host>, default:127.0.0.1, -p <port> default:6379
+    If no args , launch interactive REPL mode
+
+2. Object-Oriented Programming
+   RedisClient, CommandHandler, ResponseParser, CLI
+   
+3. Establish TCP connection to redis (redis client)   
+   Berkeley sockets to open TCP connection
+   Ipv4 and Ipv6  `getaddrinfo`
+
+4. Parsing and Command Formatting (CommandHandler)
+    Split user input
+    Convert commands into RESP format:
+
+    *3\r\n
+    $3\r\nSET\r\n
+    $3\r\nnkey\r\n
+    $5\r\nvalue\r\n  
+     
+5. Handling Redis Responses (ResponseParser)
+    Read server responses and parses RESP types
+    +OK, -Error, :100, $5\r\nhello -> Bulk string, *2\r\n$3\r\nfoo\r\n$3\r\nbar -> Array response
+
+6. Implement Interactive REPL (CLI)
+    Run loop: User Input, validate redis commands, send commands to the server, display parsed response
+    Support: help, quit
+
+7. main.cpp: parse command-line args, instantiate CLI and launch in REPL mode.
+
+Socket Programming
+Protocol Handling
+OOP principles
+CLI development
+
+
+*/
